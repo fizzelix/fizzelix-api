6cc5f29c493f70cc4a32465cf318980dc1dfe80c
Paulo Ramos <PauloRamos64@gmail.com>
2019-02-24 13:48:53 -0800

    update userSchema for passport

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const usersSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   yearsOfExperience: { type: String, required: true },
   kombuchas: [
     {
