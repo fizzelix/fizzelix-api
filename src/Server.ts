fcc2e55e1ed21f9fcb2ffe752711a78827658d6a
Paulo Ramos <PauloRamos64@gmail.com>
2019-03-01 14:59:39 -0800

    bodyparser config

--- src/Server.ts
+++ src/Server.ts
@@ -17,7 +17,7 @@ class Server {
 
   private config(): void {
     this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(Authentication.initialize());
   }
 
