7c8fa1c76614ff003ae0d4feacba083b923185eb
Paulo Ramos <PauloRamos64@gmail.com>
2019-03-09 17:00:05 -0800

    refactor Validation class

--- src/Validation.ts
+++ src/Validation.ts
@@ -1,52 +1,67 @@
 import validator from "validator";
-const { isEmpty, isEmail, isLength } = validator;
+import { isCompletelyEmpty } from "./utils";
 
-const isCompletelyEmpty = (value: any) => {
-  if (
-    value === undefined ||
-    value === null ||
-    (typeof value === "object" && Object.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0)
-  ) {
-    return true;
+const { isEmail, isLength } = validator;
+
+export class Validation {
+  public readonly GENERAL: string;
+  public readonly EMAIL: string;
+  public readonly PASSWORD: string;
+
+  public errors: {
+    general?: string;
+    email?: string;
+    password?: string;
+  };
+
+  public isValid: boolean;
+
+  constructor() {
+    // ERROR TYPES
+    this.GENERAL = "general";
+    this.EMAIL = "email";
+    this.PASSWORD = "password";
+
+    this.errors = {};
+    this.isValid = false;
   }
-  return false;
-};
 
-class Validation {
-  public validateRegistration(email: string, password: string) {
-    const errors: {
-      general?: string;
-      email?: string;
-      password?: string;
-    } = {};
-
-    if (!isEmail(email)) {
-      errors.email = "Please enter a valid Email";
+  public setErrors(message: string, errorType: string): void {
+    switch (errorType) {
+      case this.EMAIL:
+        this.errors.email = message;
+        break;
+      case this.PASSWORD:
+        this.errors.password = message;
+        break;
+      case this.GENERAL:
+        this.errors.general = message;
+        break;
+      default:
+        this.errors.general = message;
+        break;
     }
+    this.isValid = isCompletelyEmpty(this.errors);
+  }
 
+  public validateRegistration(email: string, password: string): void {
+    if (!isEmail(email)) {
+      this.errors.email = "Please enter a valid Email";
+    }
     if (!isLength(password, { min: 4, max: 30 })) {
-      errors.password = "Password must be at least 4 characters";
+      this.errors.password = "Password must be at least 4 characters";
     }
-    return { errors, isValid: isCompletelyEmpty(errors) };
+    this.isValid = isCompletelyEmpty(this.errors);
   }
 
-  public validateLogin(email: string, password: string) {
-    const errors: {
-      general?: string;
-      email?: string;
-      password?: string;
-    } = {};
-
+  public validateLogin(email: string, password: string): void {
     if (!isEmail(email)) {
-      errors.email = "Please enter a valid Email";
+      this.errors.email = "Please enter a valid Email";
+    }
+    if (isCompletelyEmpty(password)) {
+      this.errors.password = "Please enter Password";
     }
 
-    if (isEmpty(password)) {
-      errors.password = "Please enter Password";
-    }
-    return { errors, isValid: isCompletelyEmpty(errors) };
+    this.isValid = isCompletelyEmpty(this.errors);
   }
 }
-
-export default new Validation();
