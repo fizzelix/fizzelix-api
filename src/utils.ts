318a7fafd84a20890382a551c17dfa9e993bcd70
Paulo Ramos <PauloRamos64@gmail.com>
2019-03-09 16:59:26 -0800

    extract util functions

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+export const isCompletelyEmpty = (value: any): boolean => {
+  if (
+    value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  ) {
+    return true;
+  }
+  return false;
+};
