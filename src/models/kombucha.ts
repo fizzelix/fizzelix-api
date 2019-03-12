3abe0eebc7b7d7ad60ad5b0c54bff07898db7284
Paulo Ramos <PauloRamos64@gmail.com>
2019-03-11 21:14:56 -0700

    remove brewers from kombucha model

--- src/models/kombucha.ts
+++ src/models/kombucha.ts
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
 
 const kombuchaSchema = new mongoose.Schema({
-  brewers: { type: [String], required: true },
   teaType: { type: String, required: true },
   containerSize: { type: String, required: true },
   containerType: { type: String, required: true },
   mainIngredients: {
     waterInQts: { type: Number, required: true },
     sugarInCups: { type: Number, required: true },
-    teaInGrams: { type: Number, required: true }
+    teaInGrams: { type: Number, required: true },
   },
   brewDate: { type: Date, required: true },
   bottleDate: { type: Date, required: false },
   harvestDate: { type: Date, required: false },
-  notes: { type: String, required: false }
+  notes: { type: String, required: false },
 });
 
 export const Kombucha = mongoose.model("Kombucha", kombuchaSchema);
