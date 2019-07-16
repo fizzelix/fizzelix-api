d68c5ae9262c20f5bd8478fc1a3b2985e6bf104f
Paulo Ramos <PauloRamos64@gmail.com>
2019-07-15 21:38:40 -0700

    inherit validation from controller

--- src/controllers/kombuchaController.ts
+++ src/controllers/kombuchaController.ts
@@ -1,57 +1,62 @@
-import mongoose from "mongoose";
-import { Request, Response } from "express";
-import { Validation } from "../Validation";
-
-import { User } from "../models/users";
+import { Controller } from "./Controller";
 import { getKombuchaType } from "../utils/utils";
+import { Request, Response } from "express";
+import { User } from "../models/users";
 
-class KombuchaController {
-  public addNewKombucha(req: Request, res: Response) {
-    const validation = new Validation();
+class KombuchaController extends Controller {
+  constructor() {
+    super();
+  }
+
+  public addNewKombucha = (req: Request, res: Response) => {
     const { type, model } = getKombuchaType(req.params.type);
 
     User.findOne({ email: req.user.email }, (err: any, user: any) => {
       if (err) {
         console.log(err);
-        validation.setErrors("Unable to find user. Try again later...", validation.GENERAL);
-        return res.status(503).json({ error: validation.errors }); // Service  Unavailable
+        this.validation.setErrors(
+          "Unable to find user. Try again later...",
+          this.validation.GENERAL
+        );
+        return res.status(503).json({ error: this.validation.errors }); // Service  Unavailable
       }
       model.create(req.body, (err: any, kombucha: any) => {
         if (err) {
           console.log(err);
-          validation.setErrors("Unable to create kombucha. Try again later...", validation.GENERAL);
-          return res.status(503).json({ errors: validation.errors }); // Service  Unavailable
+          this.validation.setErrors(
+            "Unable to create kombucha. Try again later...",
+            this.validation.GENERAL
+          );
+          return res.status(503).json({ errors: this.validation.errors }); // Service  Unavailable
         }
         user.kombuchas[type].push(kombucha);
-        user.save((err: any, user: any) => {
+        user.save((err: any) => {
           if (err) {
             console.log(err);
-            validation.setErrors("Failed to save kombucha", validation.GENERAL);
-            return res.status(503).json({ errors: validation.errors }); // Service  Unavailable
+            this.validation.setErrors("Failed to save kombucha", this.validation.GENERAL);
+            return res.status(503).json({ errors: this.validation.errors }); // Service  Unavailable
           }
           res.json(kombucha);
         });
       });
     });
-  }
+  };
 
-  public getKombucha(req: Request, res: Response) {
-    const validation = new Validation();
+  public getKombucha = (req: Request, res: Response) => {
     const { model } = getKombuchaType(req.params.type);
     const { kombuchaId } = req.params;
 
     model.findById(kombuchaId, (err: any, kombucha: any) => {
       if (err) {
         console.log(err);
-        validation.setErrors("Failed to get kombucha", validation.GENERAL);
-        return res.status(503).json({ errors: validation.errors });
+        this.validation.setErrors("Failed to get kombucha", this.validation.GENERAL);
+        return res.status(503).json({ errors: this.validation.errors });
       }
       res.json(kombucha);
     });
-  }
+  };
 
-  public editKombucha(req: Request, res: Response) {
-    const validation = new Validation();
+  public editKombucha = (req: Request, res: Response) => {
     const { model } = getKombuchaType(req.params.type);
     const { kombuchaId } = req.params;
     const newContent = req.body;
@@ -60,15 +65,14 @@ class KombuchaController {
     model.findByIdAndUpdate(kombuchaId, newContent, { new: true }, (err: any, kombucha: any) => {
       if (err) {
         console.log(err);
-        validation.setErrors("Failed to edit kombucha", validation.GENERAL);
-        return res.status(503).json({ errors: validation.errors });
+        this.validation.setErrors("Failed to edit kombucha", this.validation.GENERAL);
+        return res.status(503).json({ errors: this.validation.errors });
       }
       res.json(kombucha);
     });
-  }
+  };
 
-  public deleteKombucha(req: Request, res: Response) {
-    const validation = new Validation();
+  public deleteKombucha = (req: Request, res: Response) => {
     const { model, type } = getKombuchaType(req.params.type);
     const { kombuchaId } = req.params;
 
@@ -76,15 +80,15 @@ class KombuchaController {
     model.findByIdAndDelete(kombuchaId, (err: any, kombucha: any) => {
       if (err) {
         console.log(err || kombucha === null);
-        validation.setErrors("Failed to find kombucha to delete", validation.GENERAL);
-        return res.status(503).json({ errors: validation.errors });
+        this.validation.setErrors("Failed to find kombucha to delete", this.validation.GENERAL);
+        return res.status(503).json({ errors: this.validation.errors });
       }
       // delete kombucha from user model
       User.findOne({ _id: req.user.id }, (err, user: any) => {
         if (err) {
           console.log("err");
-          validation.setErrors("Failed to your kombucha", validation.GENERAL);
-          return res.status(503).json({ errors: validation.errors });
+          this.validation.setErrors("Failed to your kombucha", this.validation.GENERAL);
+          return res.status(503).json({ errors: this.validation.errors });
         }
 
         const kombuchaArray = user.kombuchas[type];
@@ -97,7 +101,7 @@ class KombuchaController {
       });
       return res.json({ success: true });
     });
-  }
+  };
 }
 
 export default new KombuchaController();
